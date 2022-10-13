03e5caf991bf9751b105695db46f6c61a5187fdc
ltran7233 <ltran7233@LAPTOP-AAAUJLF3.localdomain>
2022-10-12 23:08:31 -0400

    fixed doctest and added test cases for taskB

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,10 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-#include "funcs.h"
 
-// add your tests here
+#include "doctest.h"
+#include "caesar.h"
+
+TEST_CASE("caesar rightShift tests")
+{
+	CHECK(encryptCaesar("Hello World!", 15) == "Wtaad Ldgas!");
+	
+}
